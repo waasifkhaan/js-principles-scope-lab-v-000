fa6005b523af6d62e8b1f8e45898d9a1e00326e4
Wasif Khan <waasif.khaan@gmail.com>
2018-11-01 17:20:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-function upperCaseCustomerName(customerName){
-  return toUpperCase(customerName);
+function upperCaseCustomerName(){
+  return customerName.toUpperCase();
 }
